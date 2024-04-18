ce4f026b7b3805088fef672a76206ebf0cb8e1fb
Pedro Bombig <pedro.bombig@gmail.com>
2024-04-18 07:23:32 -0300

    test: Add test Auth

---
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function test_fail_auth(): void
+    {
+        $response = $this->postJson('/auth', []);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_auth(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->postJson('/auth', [
+            'email' => $user->email,
+            'password' => 'password',
+            'device_name' => 'desktop'
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_logout(): void
+    {
+        $response = $this->postJson('/logout');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_logout(): void
+    {
+        $user = User::factory()->create();
+        $token =  $user->createToken('desktop')->plainTextToken;
+
+        $response = $this->postJson('/logout', [], [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_get_me(): void
+    {
+        $response = $this->getJson('/me');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_me(): void
+    {
+        $user = User::factory()->create();
+        $token =  $user->createToken('desktop')->plainTextToken;
+        $response = $this->getJson('/me', [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
